63d81d6ebef337ecea378f02881290df8dc0cd6a
Arun Negi <Arun-Negi989>
2025-11-24 10:38:46 +0530

    create catagory tabs

--- src/components/Dashboard/sidebar/components/Brand.js
+++ src/components/Dashboard/sidebar/components/Brand.js
@@ -1,29 +1,30 @@
 import React from "react";
-
-// Chakra imports
 import { Flex, useColorModeValue } from "@chakra-ui/react";
 
-// Custom components
-import { HorizonLogo } from "../../../../components/Dashboard/icons/Icons";
+// FIXED: default import (not named)
+import HorizonLogo from "../../../../assets/images/House_of_intimacy_logo.webp";
+
 import { HSeparator } from "../../../../components/Dashboard/separator/Separator";
 
 export function SidebarBrand() {
-  // Chakra color mode
   const logoColor = useColorModeValue("navy.700", "white");
-
-  // ⭐ Add background support for dark mode
   const bgColor = useColorModeValue("white", "navy.800");
 
   return (
     <Flex
       align="center"
       direction="column"
-      bg={bgColor}        // ⭐ Background updates in dark mode
-      borderRadius="12px" // Optional styling
+      bg={bgColor}
+      borderRadius="12px"
       py="20px"
       w="100%"
     >
-      <HorizonLogo h="26px" w="175px" my="32px" color={logoColor} />
+      <img
+        src={HorizonLogo}
+        alt="HOI Logo"
+        style={{ height: "86px", width: "225px" }}
+      />
+
       <HSeparator mb="20px" />
     </Flex>
   );
